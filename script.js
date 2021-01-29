5e82da52a4e89424db4e0658703cf7b5ebb299ed
Christopher <christophermachado5642@gmail.com>
2021-01-28 21:10:22 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,74 @@
+function mudaCor(){
+   var bloco = document.getElementById('block');
+   let r = Math.floor(Math.random()*255),
+       g = Math.floor(Math.random()*255),
+       b = Math.floor(Math.random()*255);
+   bloco.style.background = 'rgb('+r+','+g+','+b+')';
+   }
+
+   function mudaCor2(){
+    var bloco = document.getElementById('block2');
+    let r = Math.floor(Math.random()*255),
+        g = Math.floor(Math.random()*255),
+        b = Math.floor(Math.random()*255);
+    bloco.style.background = 'rgb('+r+','+g+','+b+')';
+    }
+
+    function mudaCor3(){
+        var bloco = document.getElementById('block3');
+        let r = Math.floor(Math.random()*255),
+            g = Math.floor(Math.random()*255),
+            b = Math.floor(Math.random()*255);
+        bloco.style.background = 'rgb('+r+','+g+','+b+')';
+        }
+
+    function mudaCor4(){
+            var bloco = document.getElementById('block4');
+            let r = Math.floor(Math.random()*255),
+                g = Math.floor(Math.random()*255),
+                b = Math.floor(Math.random()*255);
+            bloco.style.background = 'rgb('+r+','+g+','+b+')';
+            }
+
+    function mudaCor5(){
+                var bloco = document.getElementById('block5');
+                let r = Math.floor(Math.random()*255),
+                    g = Math.floor(Math.random()*255),
+                    b = Math.floor(Math.random()*255);
+                bloco.style.background = 'rgb('+r+','+g+','+b+')';
+                }
+
+    function mudaCor6(){
+                    var bloco = document.getElementById('block6');
+                    let r = Math.floor(Math.random()*255),
+                        g = Math.floor(Math.random()*255),
+                        b = Math.floor(Math.random()*255);
+                    bloco.style.background = 'rgb('+r+','+g+','+b+')';
+                    }
+
+    function mudaCor7(){
+                        var bloco = document.getElementById('block7');
+                        let r = Math.floor(Math.random()*255),
+                            g = Math.floor(Math.random()*255),
+                            b = Math.floor(Math.random()*255);
+                        bloco.style.background = 'rgb('+r+','+g+','+b+')';
+                        }
+
+    function mudaCor8(){
+                            var bloco = document.getElementById('block8');
+                            let r = Math.floor(Math.random()*255),
+                                g = Math.floor(Math.random()*255),
+                                b = Math.floor(Math.random()*255);
+                            bloco.style.background = 'rgb('+r+','+g+','+b+')';
+                            }
+
+    function mudaCor9(){
+                                var bloco = document.getElementById('block9');
+                                let r = Math.floor(Math.random()*255),
+                                    g = Math.floor(Math.random()*255),
+                                    b = Math.floor(Math.random()*255);
+                                bloco.style.background = 'rgb('+r+','+g+','+b+')';
+                                }
+
+                               
+
